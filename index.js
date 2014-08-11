4ed9eebae4b923ffbb7b0247fe40cac6ff6cbcb1
billautomata <bill@evident.io>
2014-08-11 10:49:26 -0700

    WB_Epsilon functionality

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var hemesh = require('./src/hemesh.js')
 hemesh.hello_world()
 console.log(hemesh.EPSILON)
 
-var p = require('./src/math/WB_Epsilon.js')
-console.log(p.EPSILON)
+var WB_Epsilon = require('./src/math/WB_Epsilon.js')
+console.log(WB_Epsilon.EPSILON)
--- src/math/WB_Epsilon.js
+++ src/math/WB_Epsilon.js
@@ -1,3 +1,22 @@
-var EPSILON = 0.000001;
+var EPSILON = 0.000001
+var SQEPSILON = EPSILON * EPSILON
 
-exports.EPSILON = EPSILON;
+/**
+ * Clamp value to range with error margin.
+ *
+ * @return clamped value
+ */
+function clampEpsilon(x, min, max) {
+	if (x < min + EPSILON) {
+		return min;
+	}
+	if (x > max - EPSILON) {
+		return max;
+	}
+	return x;
+}
+
+
+exports.EPSILON = EPSILON
+exports.SQEPSILON = SQEPSILON
+exports.clampEpsilon = clampEpsilon
